fc39c4da2884582a73d84e3eaa337c2f14c0eb15
PieterjanDC <pieterjan.de.clippel@carfac.com>
2019-07-22 09:46:17 +0200

    Add SSR

--- SpaPrerendering/ClientApp/README.md
+++ SpaPrerendering/ClientApp/README.md
@@ -1,6 +1,6 @@
-# SpaPrerendering
+# ClientApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.2.
 
 ## Development server
 
--- SpaPrerendering/Startup.cs
+++ SpaPrerendering/Startup.cs
@@ -36,16 +36,6 @@ namespace SpaPrerendering
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
 
             app.UseMvc(routes =>
             {
@@ -56,10 +46,16 @@ namespace SpaPrerendering
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
                 spa.Options.SourcePath = "ClientApp";
+
+                spa.UseSpaPrerendering(options =>
+                {
+                    options.BootModulePath = $"{spa.Options.SourcePath}/dist/server/main.js";
+                    options.BootModuleBuilder = env.IsDevelopment()
+                        ? new AngularCliBuilder(npmScript: "build:ssr")
+                        : null;
+                    options.ExcludeUrls = new[] { "/sockjs-node" };
+                });
 
                 if (env.IsDevelopment())
                 {
